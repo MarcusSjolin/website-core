a8df80d41a3d537994402e9a0a959169d72a35d1
Marcus Sjolin <marcussjolin@icloud.com>
2015-08-24 17:45:25 +0100

    Added new features

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
 var app = require("./src/Application")
-
-app.addPlugin("website-plugin-admin")
-
-app.sendMessage("preUpdateRoutes")
-app.sendMessage("updateRoutes")
-app.sendMessage("postUpdateRoutes")
-
-app.log(app)
-
--- src/Bootstrap/Plugins.js
+++ src/Bootstrap/Plugins.js
@@ -1,7 +1,17 @@
+var homePath = process.cwd()
+
 exports = module.exports = function (app) {
     var PluginsManager = require ("../Plugins/Manager")(app)
 
     app.plugins = PluginsManager.plugins
     app.addPlugin = PluginsManager.addPlugin
     app.getPlugin = PluginsManager.getPlugin
+    
+    var package = require(homePath + "/package.json")
+    
+    if (package.plugins) {
+        for (var i in package.plugins) {
+            app.addPlugin(i)
+        }
+    }
 }
