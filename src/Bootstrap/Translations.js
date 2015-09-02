c54d4616e9e879648a9e7e381269f610cd59da48
Marcus Sjolin <marcus.sjolin@ticketmaster.co.uk>
2015-09-02 16:29:00 +0100

    Updating and adding namespaces for translations, so individual components can have individual translations

--- src/Bootstrap/Translations.js
+++ src/Bootstrap/Translations.js
@@ -1,18 +1,22 @@
 module.exports = function(app) {
     var translations = {}
 
-    app.translate = function(keyword) {
-        return translations[app.locale][keyword] || keyword
+    app.translate = function(namespace, keyword) {
+        return translations[namespace][app.locale][keyword] || keyword
     }
 
-    app.addTranslations = function(locale, data) {
-        if (! translations[locale]) {
-            translations[locale] = data
+    app.addTranslations = function(namespace, locale, data) {
+        if (! translations[namespace]) {
+            translations[namespace] = {}
+            return
+        }
+        if (! translations[namespace][locale]) {
+            translations[namespace][locale] = data
             return
         }
 
         for (var i in data) {
-            translations[language][i] = data[i]
+            translations[namespace][locale][i] = data[i]
         }
     }
 
@@ -20,15 +24,11 @@ module.exports = function(app) {
         return translations
     }
 
-    app.get('/api/translations', function(req, res) {
-        res.json(translations)
+    app.get('/api/translations/:namespace', function(req, res) {
+        res.json(translations[req.params.namespace])
     })
 
-    app.get('/api/translations/:locale', function(req, res) {
-        res.json(translations[req.query.locale])
-    })
-
-    app.on("addLocaleTranslations", function(data) {
-        app.log(data)
+    app.get('/api/translations/:namespace/:locale', function(req, res) {
+        res.json(translations[req.params.namespace][req.params.locale])
     })
 }
